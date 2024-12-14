c90b315ca0e9c8d201d7876c4bba200897f90336
saiki038 <saxenaktz@gmail.com>
2024-12-14 09:16:08 +0530

    modified version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,53 @@
-import {useState} from 'react'
-import Library from '/Library.svg'
+import { useState } from 'react'
 
+
+import MusioLogo from '/MusioLogo.svg'
 
 import './styles/App.css'
 import Playlists from './Components/Playlists'
 import Header from './Components/Header'
-import LeftHomeSearch from './Components/LeftHomeSearch'
-import MusioLogo from '/MusioLogo.svg'
+import LibraryLeft from './Components/LibraryLeft'
+// import MusioLogo from '/MusioLogo.svg'
 
 
 function App() {
-
+  // useState used for background toggle
   const [mystyle, setMystyle] = useState({
     // color: 'white',
     backgroundImage: 'url(../images/Scenery.jpg)',
     backgroundSize: 'cover'
   })
 
-
+  // interchange between background
   const togglestyle = () => {
     if (mystyle.backgroundImage == 'url(../images/Scenery.jpg)') {
       setMystyle({
         backgroundImage: 'url(../images/Nightmode.jpg)',
         backgroundSize: 'cover'
       })
-
-
     }
     else {
       setMystyle({
         backgroundImage: 'url(../images/Scenery.jpg)',
         backgroundSize: 'cover'
       })
-
     }
   }
 
 
 
+  // Main container of Musio
   return (
     <>
-      <div className="container flex bg-black" style ={mystyle}>
-
+      <div className="container flex bg-black" style={mystyle}>
         <div className="left">
-          <button className="Musio-Btn" onClick = {togglestyle}>
+          <button className="Musio-Btn" onClick={togglestyle}>
             <img src={MusioLogo} alt="MusioLogo" />
           </button>
           <div className="Name">Musio</div>
-          <LeftHomeSearch/>
-          <div className="library bg-bluish rounded m-1 p-1">
-            <div className="heading">
-              <a>
-                <img src={Library} alt="Library" />
-              </a>
-              <h1>Library</h1>
-            </div>
-            <div className="Library-songs">
-            </div>
-          </div>
+          <LibraryLeft/>
         </div>
+
         <div className="right bg-bluish rounded m-1 p-1">
           <Header />
           <Playlists />
